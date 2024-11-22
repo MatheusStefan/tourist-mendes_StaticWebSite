4a728e0d96d889e5c435949431aa252b21114fcc
Matheus Stefan <roccomatheus0@gmail.com>
2024-11-22 15:51:56 -0300

    variables and reset spacing

--- UsersStaticSite/tailwind.config.js
+++ UsersStaticSite/tailwind.config.js
@@ -6,6 +6,21 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        100: "#27A4BD"
+      },
+      white: {
+        100: "#fff"
+      },
+      black: {
+        100: "#000",
+        200: "#1B1B1B"
+      },
+      red: {
+        100: "#BF1A1A"
+      }
+    }
   },
   plugins: [],
 }
